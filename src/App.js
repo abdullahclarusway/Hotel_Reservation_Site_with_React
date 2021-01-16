5f85037ed3c2ee954c5cd6ffaa6a17b13c5c07dd
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2021-01-16 23:52:10 +0100

    Add pages and router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
+import Error from "./pages/Error";
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Lets build a hotel site</h1>
-    </div>
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/rooms" component={Rooms} />
+          <Route exact path="/rooms/:slug" component={SingleRoom} />
+          <Route component={Error} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
