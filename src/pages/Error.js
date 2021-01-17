ccce099f6a79cd7d49c0a5e18e1094a06e6d6446
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2021-01-17 01:27:30 +0100

    Add hero and banner components

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,10 +1,17 @@
 import React from "react";
+import Banner from "../components/Banner";
+import Hero from "../components/Hero";
+import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
-    <div>
-      <p>Error</p>
-    </div>
+    <Hero>
+      <Banner title="404" subtitle="page not found">
+        <Link to="/" className="btn-primary">
+          return home
+        </Link>
+      </Banner>
+    </Hero>
   );
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,17 @@
 import React from "react";
+import Banner from "../components/Banner";
+import Hero from "../components/Hero";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
-      <p> Home</p>
-    </div>
+    <Hero>
+      <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
+        <Link to="/rooms" className="btn-primary">
+          our rooms
+        </Link>
+      </Banner>
+    </Hero>
   );
 };
 
