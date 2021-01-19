0c0931e09d0cfc4147138c4b4640bdef46a9bd0d
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2021-01-19 23:27:17 +0100

    Add context.js

--- src/App.js
+++ src/App.js
@@ -5,19 +5,22 @@ import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import { RoomProvider } from "./context";
 
 function App() {
   return (
     <>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/rooms" component={Rooms} />
-          <Route exact path="/rooms/:slug" component={SingleRoom} />
-          <Route component={Error} />
-        </Switch>
-      </Router>
+      <RoomProvider>
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/rooms" component={Rooms} />
+            <Route exact path="/rooms/:slug" component={SingleRoom} />
+            <Route component={Error} />
+          </Switch>
+        </Router>
+      </RoomProvider>
     </>
   );
 }
